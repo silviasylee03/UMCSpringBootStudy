4aac8eff8a212a245ca5fe02cc29e559de933e49
이소연 <122617471+silviasylee03@users.noreply.github.com>
2025-05-10 21:08:29 +0900

    feat: memberQueryService 추가

--- src/main/java/umc/spring/service/MemberService/MemberQueryService.java
+++ src/main/java/umc/spring/service/MemberService/MemberQueryService.java
@@ -1,5 +1,12 @@
 package umc.spring.service.MemberService;
 
-public interface MemberQueryService {
+import umc.spring.domain.Member;
+import umc.spring.web.dto.MyPageDto;
 
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberQueryService {
+    Optional<Member> findMember(Long id);
+    MyPageDto findMembersById(Long id);
 }
