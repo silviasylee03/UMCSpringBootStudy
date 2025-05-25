c65503b0ede5426313dab9c92d8e1e6d717e56d1
이소연 <122617471+silviasylee03@users.noreply.github.com>
2025-05-25 19:48:05 +0900

    feat #10: 9주차 미션 내가 작성한 리뷰 조회 API

--- src/main/java/umc/spring/service/MemberService/MemberQueryService.java
+++ src/main/java/umc/spring/service/MemberService/MemberQueryService.java
@@ -1,6 +1,8 @@
 package umc.spring.service.MemberService;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Member;
+import umc.spring.domain.Review;
 import umc.spring.web.dto.MyPageDto;
 
 import java.util.List;
@@ -9,4 +11,5 @@ import java.util.Optional;
 public interface MemberQueryService {
     Optional<Member> findMember(Long id);
     MyPageDto findMembersById(Long id);
+    Page<Review> getMemberReviewList(Long memberId, Integer page);
 }
